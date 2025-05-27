41b8ea466ae8e983754e57594b3bb27c51c56cc3
mohand-b <mohandbdj@gmail.com>
2025-05-27 13:42:38 +0200

    Initial backend structure: entities, services, controllers for Visitor, Admin, Timeline, Achievement, Contact, Question

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VisitorController } from './visitor/visitor.controller';
+import { VisitorService } from './visitor/visitor.service';
+import { AdminController } from './admin/admin.controller';
+import { AdminService } from './admin/admin.service';
+import { TimelineController } from './timeline/timeline.controller';
+import { TimelineService } from './timeline/timeline.service';
+import { AchievementController } from './achievement/achievement.controller';
+import { AchievementService } from './achievement/achievement.service';
+import { ContactController } from './contact/contact.controller';
+import { ContactService } from './contact/contact.service';
+import { QuestionController } from './question/question.controller';
+import { QuestionService } from './question/question.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, VisitorController, AdminController, TimelineController, AchievementController, ContactController, QuestionController],
+  providers: [AppService, VisitorService, AdminService, TimelineService, AchievementService, ContactService, QuestionService],
 })
 export class AppModule {}
